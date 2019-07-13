3a79fe2a54bec2f700c72b9b37abf44d2a352010
Hex <hex4nova@gmail.com>
2019-07-13 17:23:48 +0200

    [style] help.js formatting

--- cmd/util/help.js
+++ cmd/util/help.js
@@ -10,7 +10,7 @@ const helpText = require("./help.json");
 
       if (helpText[args[0]].redirect !== undefined) { // exception if the searched command is an alias
         command = helpText[helpText[args[0]].redirect];
-        redirect = "*Redirected from "+args[0].toLowerCase()+"*";
+        redirect = "*Redirected from `."+args[0].toLowerCase()+"`*";
       }
 
       let description = command.description;
@@ -25,21 +25,20 @@ const helpText = require("./help.json");
       }
       let roles = "";
       if (command.roles !== undefined) { // ingame commands
-        roles = "\n\n**Can be used by:** "+command.roles;
+        roles = "\n**Can be used by:** "+command.roles;
       }
       let alias = "";
       if (command.alias !== undefined) { // commands with aliases
-        alias = "\n(Alias: "+command.alias+")";
+        alias = "\n**Alias:** "+command.alias;
       }
 
       const helpEmbed = new Discord.RichEmbed()
         .setColor('#11001e')
-        .setTitle('**'+command.name+'**')
+        .setTitle('**.'+command.name+'**')
         .setAuthor('Mayhem City: Mafia', 'https://cdn.discordapp.com/avatars/462545110144516096/1c45ebd0c04974f3f3dc1ee5b01d30c7.png', 'http://hex4nova.cf/mafia')
         .setDescription(redirect)
-        .addField('*Description*', description+alias)
-        .addField('*Usage*', command.usage+roles)
-        .setTimestamp()
+        .addField('Description', description)
+        .addField('Usage', command.usage+"\n"+roles+alias)
 
       message.channel.send(helpEmbed);
 
@@ -52,15 +51,15 @@ const helpText = require("./help.json");
 
     const helpEmbed = new Discord.RichEmbed()
       .setColor('#11001e')
-      .setTitle('Command help')
+      .setTitle('**Command help**')
       .setAuthor('Mayhem City: Mafia', 'https://cdn.discordapp.com/avatars/462545110144516096/1c45ebd0c04974f3f3dc1ee5b01d30c7.png', 'http://hex4nova.cf/mafia')
       .setDescription('Do `.help <command>` for more information about the command.')
       .addField('*Ingame*', '`.exe` `.i` `.lastwill` `.lw` `.reveal` `.w` `.whisper`')
       .addField('*Documentation*', '`.r` `.rl` `.role` `.roles` `.rolelist`')
-      .addField('**Utility**', '`.alert` `.gmt` `.help` `.ping` `.signups` `.utc`')
-      .addField('**Interactions**', '`.handshake` `.hit` `.hug` `.slap` `.spray`')
-      .addField('**Self**', '`.seize`')
-      .addField('**Fun**', '`.8ball` `.catfact` `.coin` `.ligma` `.rps`')
+      .addField('*Utility*', '`.alert` `.gmt` `.help` `.ping` `.signups` `.utc`')
+      .addField('*Interactions*', '`.handshake` `.hit` `.hug` `.slap` `.spray`')
+      .addField('*Self*', '`.seize`')
+      .addField('*Fun*', '`.8ball` `.catfact` `.coin` `.ligma` `.rps`')
       .setTimestamp()
 
     message.channel.send(helpEmbed);
