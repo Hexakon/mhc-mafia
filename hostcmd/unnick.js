9ffbfa321e257767290effa3ef283489279b37fe
Hex <hex4nova@gmail.com>
2019-08-28 20:05:24 +0200

    [fix] awaits in unnick.js

--- hostcmd/unnick.js
+++ hostcmd/unnick.js
@@ -4,13 +4,15 @@ exports.run = (client, message, args) => {
   const deadRole = message.guild.roles.find('name', 'Dead')
 
   message.channel.send("Removing ingame roles...");
-  for (let [sf, user] of aliveRole.members) {
-    user.removeRole(aliveRole);
-    user.setNickname('');
-  }
-  for (let [sf, user] of deadRole.members) {
-    user.removeRole(deadRole);
-    user.setNickname('');
-  }
-  
+  (async () => {
+    for (let [sf, user] of aliveRole.members) {
+      await user.removeRole(aliveRole);
+      await user.setNickname('');
+    }
+    for (let [sf, user] of deadRole.members) {
+      await user.removeRole(deadRole);
+      await user.setNickname('');
+    }
+  })();
+
 }
