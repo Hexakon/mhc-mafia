ee88d524c7469c872f12297f7293846b5c246cca
Hex <hex4nova@gmail.com>
2019-08-28 11:35:25 +0200

    [fix/feat] debug command for server category reload

---
+++ hostcmd/loadcategories.js
@@ -0,0 +1,15 @@
+exports.run = (client, message, args) => {
+  const dataChannel = require.main.require("./data/channel.json");
+  const fnChannel = process.cwd() + "/data/channel.json";
+
+  const $function = require.main.require("./const/function.js");
+
+  dataChannel.publicCategory = process.env.PUBCAT
+  dataChannel.privateCategory = process.env.PRICAT
+  dataChannel.deadCategory = process.env.DEADCAT
+
+  message.channel.send(dataChannel.publicCategory + " " + dataChannel.privateCategory + " " + dataChannel.deadCategory)
+
+
+  $function.writeFile(fnChannel, dataChannel)
+}
