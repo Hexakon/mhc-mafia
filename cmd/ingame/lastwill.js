d92fc48c8ed50f69d5ee55aac83af1e83b3616b4
Hex <hex4nova@gmail.com>
2020-04-29 18:19:42 +0200

    [fix] catch-up update for lastwill ingame command

--- cmd/ingame/lastwill.js
+++ cmd/ingame/lastwill.js
@@ -1,67 +1,62 @@
 exports.run = (client, message, args) => {
-  const fs = require('fs');
-  const data = require(__dirname + "/../../data/id.json");
-  const dataLW_fn = __dirname + "/../../data/will.json";
-  const dataLW = require(dataLW_fn);
-  const dataID = data.id
-  let user;
+  const dataPlayer = require.main.require("./data/player.json");
+  const dataWill = require.main.require("./data/lastwill.json");
+  const fnWill = process.cwd() + "/data/will.json";
 
-  if  (message.member.roles.find("name", "Alive") || message.member.roles.find("name", "Dead")) {
+  const $function = require.main.require("./const/function.js");
+
+  if (message.member.roles.find("name", "Alive") || message.member.roles.find("name", "Dead")) {
   if (message.channel.parentID === "447858869058928642") {
 
-    function getKey(object, value) {
-      return Object.keys(object).find(key => object[key] === value);
-    }
-
-    user = getKey(dataID, message.author.id);
+    let user = Object.keys(dataPlayer).find(key => dataPlayer[key] === message.author.id);
 
     if (args[0] == "view" || args[0] == "read") {
-      if (dataLW[user] == "") {
+      if (dataWill[user] == "") {
         message.channel.send("**:warning: You don't have a Last Will.**\nTo write one, use `.lastwill <write/add> <text>`.");
       } else {
-        message.channel.send("**:scroll: Your Last Will is currently set to:**```md\n" + dataLW[user] + "```");
+        message.channel.send("**:scroll: Your Last Will is currently set to:**```md\n" + dataWill[user] + "```");
       }
     } else if (args[0] == "rewrite" || args[0] == "overwrite") {
       if (args.slice(1).join(" ") == "") {
         message.channel.send("**:warning: You cannot write a Last Will with no content. Please use `.lastwill <clear/delete>` to delete your Last Will.**");
       } else {
-        dataLW[user] = args.slice(1).join(" ");
-        while (dataLW[user].charAt(0) == "\\" && dataLW[user].charAt(1) == "n") { // remove linebreaks from the start of the string.
-          dataLW[user] = dataLW[user].substring(2);
+        dataWill[user] = args.slice(1).join(" ");
+        while (dataWill[user].charAt(0) == "\\" && dataWill[user].charAt(1) == "n") { // remove linebreaks from the start of the string.
+          dataWill[user] = dataWill[user].substring(2);
         }
-        if (dataLW[user].length > 800) {
-          message.channel.send(`**:warning: Your Last Will was not saved because it exceeded 800 characters! (Current length: ${dataLW[user].length} characters)`);
+        if (dataWill[user].length > 800) {
+          message.channel.send(`**:warning: Your Last Will was not saved because it exceeded 800 characters! (Current length: ${dataWill[user].length} characters)`);
         } else {
-          message.channel.send("**:scroll: Your Last Will has been updated to:**```md\n" + dataLW[user] + "```");
+          message.channel.send("**:scroll: Your Last Will has been updated to:**```md\n" + dataWill[user] + "```");
 
-          fs.writeFile(dataLW_fn, JSON.stringify(dataLW, null, 2), function (err) {if (err) return console.log(err);});
+          $function.writeFile(fnWill, dataWill)
         }
       }
 
     } else if (args[0] == "add" || args[0] == "write") {
       if (args.slice(1).join(" ") == "") {
-        dataLW[user] = dataLW[user] + "\n";
+        dataWill[user] = dataWill[user] + "\n";
       } else {
-        dataLW[user] = dataLW[user] + "\n" + args.slice(1).join(" ");
-        while (dataLW[user].charAt(0) == "\\" && dataLW[user].charAt(1) == "n") { // remove linebreaks from the start of the string.
-          dataLW[user] = dataLW[user].substring(2);
+        dataWill[user] = dataWill[user] + "\n" + args.slice(1).join(" ");
+        while (dataWill[user].charAt(0) == "\\" && dataWill[user].charAt(1) == "n") { // remove linebreaks from the start of the string.
+          dataWill[user] = dataWill[user].substring(2);
         }
       }
-      if (dataLW[user].length > 800) {
-        message.channel.send(`**:warning: Your Last Will was not saved because it exceeded 800 characters!** (Current length: ${dataLW[user].length} characters)`);
+      if (dataWill[user].length > 800) {
+        message.channel.send(`**:warning: Your Last Will was not saved because it exceeded 800 characters!** (Current length: ${dataWill[user].length} characters)`);
       } else {
-        message.channel.send("**:scroll: Your Last Will has been updated to:**```md\n" + dataLW[user] + "```");
+        message.channel.send("**:scroll: Your Last Will has been updated to:**```md\n" + dataWill[user] + "```");
 
-        fs.writeFile(dataLW_fn, JSON.stringify(dataLW, null, 2), function (err) {if (err) return console.log(err);});
+        $function.writeFile(fnWill, dataWill)
       }
 
     } else if (args[0] == "clear" || args[0] == "delete") {
 
-      message.channel.send("**Your Last Will has been deleted.**\nUse the copy below for manual recovery if necessary:```md\n"+dataLW[user]+"```");
+      message.channel.send("**Your Last Will has been deleted.**\nUse the copy below for manual recovery if necessary:```md\n"+dataWill[user]+"```");
 
-      dataLW[user] = "";
+      dataWill[user] = "";
 
-      fs.writeFile(dataLW_fn, JSON.stringify(dataLW, null, 2), function (err) {if (err) return console.log(err);});
+      $function.writeFile(fnWill, dataWill)
 
     } else if (args[0] == "debug") {
       message.channel.send("User ID: "+user);
